ff674fca4c21ac63c396a87d559a999edf8376c2
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 13:12:07 -0400

    Update DatabaseResponse.ts

--- server/api/controllers/DatabaseResponse.ts
+++ server/api/controllers/DatabaseResponse.ts
@@ -1,7 +1,9 @@
 import { DatabaseConfig } from '../../database/DatabaseConfig';
 import { DatabaseStatus } from '../../types/DatabaseStatus';
+import { DatabaseVersion } from '../../types/DatabaseVersion';
 
 export interface DatabaseResponse {
   config: DatabaseConfig;
   status: DatabaseStatus;
+  version: DatabaseVersion;
 }
