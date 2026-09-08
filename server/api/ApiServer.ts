d9bf34ab8c78d382d3adf3e43388f99345632245
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 11:15:13 -0400

    Update ApiServer.ts

--- server/api/ApiServer.ts
+++ server/api/ApiServer.ts
@@ -1,12 +1,21 @@
-import { ApiStatus } from './ApiStatus';
 import { ApiConfig } from './ApiConfig';
+import { ApiRoutes } from './ApiRoutes';
+import { ApiStatus } from './ApiStatus';
 
 export class ApiServer {
+  private routes = new ApiRoutes();
+
   async start(
     config: ApiConfig
   ): Promise<ApiStatus> {
+    const routes = this.routes.getRoutes();
+
     console.log(
       `API Server starting on port ${config.port}...`
+    );
+
+    console.log(
+      `Registered ${routes.length} routes`
     );
 
     return {
