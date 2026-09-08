72452ad5a060b3071302ca067ac690d00c2d4a2f
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 11:30:42 -0400

    Update GameController.ts

--- server/api/controllers/GameController.ts
+++ server/api/controllers/GameController.ts
@@ -1,9 +1,23 @@
 import { GameService } from '../../services/GameService';
+import { ControllerResponse } from './ControllerResponse';
+import { Game } from '../../types/Game';
 
 export class GamesController {
   private gameService = new GameService();
 
-  async getGames() {
-    return this.gameService.getAllGames();
+  async getGames(): Promise<ControllerResponse<Game[]>> {
+    const result = await this.gameService.getAllGames();
+
+    if (!result.success) {
+      return {
+        success: false,
+        error: result.error,
+      };
+    }
+
+    return {
+      success: true,
+      data: result.data,
+    };
   }
 }
