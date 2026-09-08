4281546ce106b128d6355385728b31b97afde91e
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 12:05:58 -0400

    Create GameStatisticsController.ts

---
+++ server/api/controllers/GameStatisticsController.ts
@@ -0,0 +1,34 @@
+import { GameStatisticsService } from '../../services/GameStatisticsService';
+import { ControllerResponse } from './ControllerResponse';
+import { ControllerCodes } from './ControllerCodes';
+import { GameSummaryResponse } from './GameSummaryResponse';
+
+export class GameStatisticsController {
+  private gameStatisticsService =
+    new GameStatisticsService();
+
+  async getSummary(): Promise<
+    ControllerResponse<GameSummaryResponse>
+  > {
+    try {
+      const summary =
+        await this.gameStatisticsService.getSummary();
+
+      return {
+        success: true,
+        data: {
+          summary,
+        },
+      };
+    } catch {
+      return {
+        success: false,
+        error: {
+          code: ControllerCodes.GameError,
+          message:
+            'Unable to retrieve game statistics',
+        },
+      };
+    }
+  }
+}
