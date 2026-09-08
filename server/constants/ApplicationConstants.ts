c7502069daaaf6a7a519f98f3a27091e3f481fa3
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 13:32:27 -0400

    Update ApplicationConstants.ts

--- server/constants/ApplicationConstants.ts
+++ server/constants/ApplicationConstants.ts
@@ -1,6 +1,8 @@
 import { ApplicationInfo } from '../types/ApplicationInfo';
+import { ApplicationVersion } from '../types/ApplicationVersion';
 
-export const ApplicationConstants: ApplicationInfo = {
+export const ApplicationConstants:
+  ApplicationInfo & ApplicationVersion = {
   name: 'Gamerr',
   version: '0.1.0-alpha',
 };
