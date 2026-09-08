6a5fb742109307d7e58829ae932e7a4f07d635f8
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 10:59:26 -0400

    Update GameService.ts

--- server/services/GameService.ts
+++ server/services/GameService.ts
@@ -10,4 +10,18 @@ export class GameService {
   async getGameById(id: number) {
     return this.gameRepository.getById(id);
   }
+
+  async createGame(
+    title: string,
+    platform: string,
+    path: string,
+    fileSize?: bigint | null
+  ) {
+    return this.gameRepository.create({
+      title,
+      platform,
+      path,
+      fileSize,
+    });
+  }
 }
