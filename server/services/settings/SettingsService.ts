ec5232e07b3728f81cb762b5ff25eb861b30a949
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 10:59:43 -0400

    Update SettingsService.ts

--- server/services/settings/SettingsService.ts
+++ server/services/settings/SettingsService.ts
@@ -1,13 +1,17 @@
 import { SettingRepository } from '../repositories/SettingRepository';
 
 export class SettingsService {
-  private settingRepository = new SettingRepository();
+  private settingRepository =
+    new SettingRepository();
 
   async getValue(key: string) {
     return this.settingRepository.getByKey(key);
   }
 
-  async setValue(key: string, value: string) {
+  async setValue(
+    key: string,
+    value: string
+  ) {
     return this.settingRepository.setValue(
       key,
       value
