af259061a57fc9fd958845325993f949360e1afe
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 10:53:02 -0400

    Update SettingRepository.ts

--- server/repositories/SettingRepository.ts
+++ server/repositories/SettingRepository.ts
@@ -1,7 +1,11 @@
 import prisma from '../database/client';
 
 export class SettingRepository {
-  async getByKey(key: string) {}
+  async getByKey(key: string) {
+    return prisma.setting.findUnique({
+      where: { key },
+    });
+  }
 
   async setValue(key: string, value: string) {}
 }
