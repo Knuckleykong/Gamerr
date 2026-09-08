d9dcf3c2bc2ae6a8643d8fd927e2e7f142008f81
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 14:05:39 -0400

    Update NodeHttpServer.ts

--- server/api/NodeHttpServer.ts
+++ server/api/NodeHttpServer.ts
@@ -1,6 +1,34 @@
+import { createServer } from 'http';
+
 import { HttpRequestHandler } from './HttpRequestHandler';
 
 export class NodeHttpServer {
   private requestHandler =
     new HttpRequestHandler();
+
+  start(port: number): void {
+    const server = createServer(
+      async (request, response) => {
+        const result =
+          await this.requestHandler.handle(
+            request.method ?? 'GET',
+            request.url ?? '/'
+          );
+
+        response.writeHead(
+          result.statusCode,
+          {
+            'Content-Type':
+              'application/json',
+          }
+        );
+
+        response.end(
+          JSON.stringify(result.body)
+        );
+      }
+    );
+
+    server.listen(port);
+  }
 }
