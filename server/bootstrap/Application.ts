cb74494ffff29ccadd790afe571fe47c250ccd93
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 09:12:12 -0400

    Update Application.ts

--- server/bootstrap/Application.ts
+++ server/bootstrap/Application.ts
@@ -1,5 +1,14 @@
+import { ConfigService } from '../config/ConfigService';
+
 export class Application {
+  private configService = new ConfigService();
+
   async start() {
     console.log('Starting Gamerr...');
+
+    const appConfig = this.configService.getAppConfig();
+
+    console.log(`Environment: ${appConfig.environment}`);
+    console.log(`Port: ${appConfig.port}`);
   }
 }
