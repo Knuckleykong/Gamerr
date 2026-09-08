be5861dcca4019d0313f6b744a73f9e21e732ca1
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 14:41:34 -0400

    Create ControllerResponseMapper.ts

---
+++ server/api/controllers/ControllerResponseMapper.ts
@@ -0,0 +1,20 @@
+import { HttpResponse } from './HttpResponse';
+import { HttpResponseFactory } from './HttpResponseFactory';
+
+import { ControllerResponse } from './controllers/ControllerResponse';
+
+export class ControllerResponseMapper {
+  static map<T>(
+    response: ControllerResponse<T>
+  ): HttpResponse {
+    if (!response.success) {
+      return HttpResponseFactory.internalServerError(
+        response
+      );
+    }
+
+    return HttpResponseFactory.ok(
+      response
+    );
+  }
+}
