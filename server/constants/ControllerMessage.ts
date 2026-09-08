2673c87649182b81097857f245873d2a13d0233b
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 13:27:56 -0400

    Create ControllerMessage.ts

---
+++ server/constants/ControllerMessage.ts
@@ -0,0 +1,10 @@
+export const ControllerMessages = {
+  HealthUnavailable:
+    'Unable to retrieve health status',
+
+  DatabaseUnavailable:
+    'Unable to retrieve database information',
+
+  GameStatisticsUnavailable:
+    'Unable to retrieve game statistics',
+};
