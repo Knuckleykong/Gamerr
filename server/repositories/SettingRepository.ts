885d8c4cc399472ccb973f4898603c8e532ca7c5
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 10:58:10 -0400

    Update SettingRepository.ts

--- server/repositories/SettingRepository.ts
+++ server/repositories/SettingRepository.ts
@@ -1,4 +1,5 @@
 import prisma from '../database/client';
+import { Setting } from '../types/Setting';
 import { RepositoryResult } from './RepositoryResult';
 
 export class SettingRepository {
@@ -16,6 +17,34 @@ export class SettingRepository {
           error: 'Setting not found',
         };
       }
+
+      return {
+        success: true,
+        data: setting,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: String(error),
+      };
+    }
+  }
+
+  async setValue(
+    key: string,
+    value: string
+  ): Promise<RepositoryResult<Setting>> {
+    try {
+      const setting = await prisma.setting.upsert({
+        where: { key },
+        update: {
+          value,
+        },
+        create: {
+          key,
+          value,
+        },
+      });
 
       return {
         success: true,
