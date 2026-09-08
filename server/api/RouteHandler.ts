fdfe666ec17f643d79321ab12690675a4e71f5e8
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 13:45:31 -0400

    Update RouteHandler.ts

--- server/api/RouteHandler.ts
+++ server/api/RouteHandler.ts
@@ -1,4 +1,4 @@
-export interface RouteHandler {
-  method: string;
-  path: string;
+import { RouteInfo } from '../types/RouteInfo';
+
+export interface RouteHandler extends RouteInfo {
   handler: () => Promise
