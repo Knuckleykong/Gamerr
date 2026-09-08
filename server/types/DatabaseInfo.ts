261e06287a6c7010da63561e35b962497c027a75
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-08 13:10:17 -0400

    Update DatabaseInfo.ts

--- server/types/DatabaseInfo.ts
+++ server/types/DatabaseInfo.ts
@@ -1,7 +1,9 @@
 import { DatabaseConfig } from '../database/DatabaseConfig';
 import { DatabaseStatus } from './DatabaseStatus';
+import { DatabaseVersion } from './DatabaseVersion';
 
 export interface DatabaseInfo {
   config: DatabaseConfig;
   status: DatabaseStatus;
+  version: DatabaseVersion;
 }
