6a27a6354659c4208ae9acbc3bd0320186a8af00
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-09 12:38:02 -0400

    Update HttpContextFactory.ts

--- server/api/HttpContextFactory.ts
+++ server/api/HttpContextFactory.ts
@@ -6,17 +6,20 @@ import { HttpRequestInfo } from '../types/HttpRequestInfo';
 export class HttpContextFactory {
   static create(
     method: string,
-    path: string
+    path: string,
+    body?: string
   ): HttpRequestContext {
     return {
       method: method as HttpMethod,
       path,
+      body,
     };
   }
 
   static createRequestInfo(
     method: string,
-    path: string
+    path: string,
+    body?: string
   ): HttpRequestInfo {
     return {
       request: this.create(
