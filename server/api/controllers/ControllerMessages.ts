fd862e09163ca689e5f5493c0d6431feb2fd6958
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-09 09:03:43 -0400

    Create ControllerMessages.ts

---
+++ server/api/controllers/ControllerMessages.ts
@@ -0,0 +1,10 @@
+export class ControllerMessages {
+  static readonly HealthUnavailable =
+    'Health information unavailable';
+
+  static readonly DatabaseUnavailable =
+    'Database information unavailable';
+
+  static readonly GameStatisticsUnavailable =
+    'Game statistics unavailable';
+}
