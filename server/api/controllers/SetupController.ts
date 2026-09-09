ab5144fa6d94a03068c65aa9e36bec29687f9701
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-09 11:43:07 -0400

    Update SetupController.ts

--- server/api/controllers/SetupController.ts
+++ server/api/controllers/SetupController.ts
@@ -1,24 +1,31 @@
+import { HttpResponse } from '../HttpResponse';
+import { HttpResponseFactory } from '../HttpResponseFactory';
+
 export class SetupController {
-  async getSetup(): Promise<string> {
-    return `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>Gamerr Setup</title>
-        </head>
+  async getSetup(): Promise<HttpResponse> {
+    return HttpResponseFactory.html(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gamerr Setup</title>
+  </head>
 
-        <body>
-          <h1>Welcome to Gamerr</h1>
+  <body>
+    <h1>Welcome to Gamerr</h1>
 
-          <p>
-            Gamerr has not been configured.
-          </p>
+    <p>
+      Gamerr has not been configured.
+    </p>
 
-          <button>
-            Begin Setup
-          </button>
-        </body>
-      </html>
-    `;
+    <p>
+      Setup wizard coming soon.
+    </p>
+
+    <button>
+      Begin Setup
+    </button>
+  </body>
+</html>
+    `);
   }
 }
