c84ef0695b9ce8a74535f88bf6d4df2dff7b7709
Knuckleykong <knuckleykonggaming@gmail.com>
2026-09-09 12:10:44 -0400

    Create MetadataSetupController.ts

---
+++ server/api/controllers/MetadataSetupController.ts
@@ -0,0 +1,64 @@
+import { HttpResponse } from '../HttpResponse';
+import { HttpResponseFactory } from '../HttpResponseFactory';
+
+export class MetadataSetupController {
+  async getSetup(): Promise<HttpResponse> {
+    return HttpResponseFactory.html(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gamerr Setup</title>
+  </head>
+
+  <body>
+    <h1>Gamerr Setup</h1>
+
+    <p>
+      Step 2 of 4
+    </p>
+
+    <h2>Metadata Configuration</h2>
+
+    <p>
+      Select a metadata provider.
+    </p>
+
+    <p>
+      <input
+        type="radio"
+        id="screenscraper"
+        name="metadata"
+        checked
+      />
+
+      <label for="screenscraper">
+        ScreenScraper
+      </label>
+    </p>
+
+    <p>
+      <input
+        type="radio"
+        id="thegamesdb"
+        name="metadata"
+      />
+
+      <label for="thegamesdb">
+        TheGamesDB
+      </label>
+    </p>
+
+    <p>
+      <button onclick="window.location.href='/setup'">
+        Back
+      </button>
+
+      <button>
+        Save and Continue
+      </button>
+    </p>
+  </body>
+</html>
+    `);
+  }
+}
